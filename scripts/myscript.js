c5865d33bd55880281ff5411c39a597cf43c70cc
Mohini Bhave <mb5157@columbia.edu>
2023-12-14 15:43:03 -0500

    Interactive plot graph added

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -3,54 +3,72 @@ const dataUrl = './data/nutrition_data.json';
 
 // Load the data
 d3.json(dataUrl).then(function(data) {
-  // Set the dimensions of the canvas/graph
-  const margin = { top: 20, right: 20, bottom: 30, left: 40 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+  // Filter the data to remove entries with values less than 4%
+  data = data.filter(d => d.underweight_percentage >= 4);
 
-  // Set the ranges with scaling
-  const x = d3.scaleLinear().range([0, width]);
-  const y = d3.scaleOrdinal().range([height, 0]);
+  // Specify the dimensions of the chart.
+  const width = 1600; /* 4 times larger */
+  const height = 1200; /* 4 times larger */
+  const margin = 1; // to avoid clipping the root circle stroke
+  const names = d => d.country.split(" "); // Modify data accessors as needed
 
-  // Define the SVG element
+  // Number format for values
+  const format = d3.format(",d");
+
+  // Create a categorical color scale.
+  const color = d3.scaleOrdinal(d3.schemeSet3);
+
+  // Create the pack layout.
+  const pack = d3.pack()
+      .size([width - margin * 2, height - margin * 2])
+      .padding(3);
+
+  // Compute the hierarchy from the (flat) data; expose the values
+  // for each node; lastly apply the pack layout.
+  const root = pack(d3.hierarchy({children: data})
+      .sum(d => d.underweight_percentage));
+
+  // Create the SVG container.
   const svg = d3.select("#plot")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-margin, -margin, width, height])
+      .attr("style", "max-width: 100%; height: auto; font: 16px sans-serif;") /* Adjust the font size */
+      .attr("text-anchor", "middle");
 
-  // Set domains for the scales
-  x.domain([0, d3.max(data, d => d.underweight_percentage)]);
-  y.domain(data.map(d => d.country));
+  // Place each (leaf) node according to the layout’s x and y values.
+  const node = svg.append("g")
+    .selectAll()
+    .data(root.leaves())
+    .join("g")
+      .attr("transform", d => `translate(${d.x},${d.y})`);
 
-  // Create a tooltip
-  const tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+  // Add a title.
+  node.append("title")
+      .text(d => `${d.data.country}\n${format(d.data.underweight_percentage)}%`);
 
-  // Draw the bubble plot with smaller points and a different color
-  svg.selectAll("circle")
-    .data(data)
-    .enter().append("circle")
-      .attr("cx", d => x(d.underweight_percentage))
-      .attr("cy", (d, i) => y(d.country))
-      .attr("r", 3)  // Smaller radius for points
-      .style("fill", "steelblue")  // Use a different color
-      .on("mouseover", (event, d) => {
-        tooltip.transition().duration(200).style("opacity", .9);
-        tooltip.html(`${d.country}: ${d.underweight_percentage}%`)
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
-      })
-      .on("mouseout", () => tooltip.transition().duration(500).style("opacity", 0));
+  // Add a filled circle.
+  node.append("circle")
+      .attr("fill-opacity", 0.7)
+      .attr("fill", d => color(d.data.country))
+      .attr("r", d => d.r);
 
-  // Add the X Axis
-  svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+  // Add a label.
+  const text = node.append("text")
+      .attr("clip-path", d => `circle(${d.r})`);
 
-  // Add the Y Axis
-  svg.append("g")
-      .call(d3.axisLeft(y));
+  // Add a tspan for each word in the country name.
+  text.selectAll()
+    .data(d => names(d.data))
+    .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.35}em`)
+      .text(d => d);
+
+  // Add a tspan for the node’s value.
+  text.append("tspan")
+      .attr("x", 0)
+      .attr("y", d => `${names(d.data).length / 2 + 0.35}em`)
+      .attr("fill-opacity", 0.7)
+      .text(d => `${format(d.data.underweight_percentage)}%`);
 });
